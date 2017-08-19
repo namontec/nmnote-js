6ecacb3b10f0e8306c0b4d1fd6ee1dd1d517dc75
Victor Potasyev <vpotasyev@gmail.com>
2017-08-19 12:04:14 +0300

    finish note class

--- nmnote.js
+++ nmnote.js
@@ -1,11 +1,31 @@
-function Note(content){
-  this.content = content;
+// Note class
+function Note(title, content) {
+  this.noteTitle = title;
+  this.noteContent = content;
 }
 Note.prototype.show = function () {
-  return this.content;
+  return (this.noteTitle + "\n" + this.noteContent);
 }
+Object.defineProperty(Note.prototype, "title", {
+  get: function () {
+    return this.noteTitle;
+  },
+  set: function (value) {
+    this.noteTitle = value;
+  }
+})
+Object.defineProperty(Note.prototype, "content", {
+  get: function() {
+    return this.noteTitle;
+  },
+  set: function(value) {
+    this.noteContent = value;
+  }
+})
 
 
 //===================
-var newNote = new Note("First note");
+var newNote = new Note("Header", "First note");
+newNote.title = "one";
+newNote.content = "other";
 console.log(newNote.show());
